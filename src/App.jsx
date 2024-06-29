72c0763d7ff2d0ab53818fa823e95d56abd0eca7
Ashish1568 <ar896926@gmail.com>
2024-06-29 18:17:02 +0530

    setup vite project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        
+        <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
